a1427592cae405683ea243825e73ce073f4bd343
ayishahudakt <24mcag01@meaec.edu.in>
2025-01-08 12:22:14 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,27 +1,15 @@
--- Create the database
 CREATE DATABASE db4;
--- Use the database
 USE db4;
--- Create the table
 CREATE TABLE users_table(
     username VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL
 );
--- Insert data into the table
 INSERT INTO users_table (username, password) 
 VALUES ('user', '1234');
 
--------------------------------------------------------------------------
--------------------------------------------------------------------------
 
 
-CREATE database user table;
-CREATE TABLE users_table(
-username varchar(20) NOT NULL,
-password varchar(20) NOT NULL
-);
-INSERT INTO users_table (username, password) VALUES
-('user', '1234');
+
 
 
 
